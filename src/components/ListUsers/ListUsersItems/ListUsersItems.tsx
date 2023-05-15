e34d40a85e31f2c674a14c2aadf9f07e99cc50dc
fefwee <aweo.wew@mail.ru>
2023-05-15 23:25:04 +0300

    three

--- src/components/ListUsers/ListUsersItems/ListUsersItems.tsx
+++ src/components/ListUsers/ListUsersItems/ListUsersItems.tsx
@@ -75,26 +75,7 @@ export const ListUsersItems: React.FC = () => {
                     </div>
                     <p>time</p>
                 </li>
-                <li className={style.user_items}>
-                    <div className={style.img_user}>
-                        <img src={logo} alt="logo_user" />
-                    </div>
-                    <div className={style.text_user}>
-                        <p>name</p>
-                        <p>message</p>
-                    </div>
-                    <p>time</p>
-                </li>
-                <li className={style.user_items}>
-                    <div className={style.img_user}>
-                        <img src={logo} alt="logo_user" />
-                    </div>
-                    <div className={style.text_user}>
-                        <p>name</p>
-                        <p>message</p>
-                    </div>
-                    <p>time</p>
-                </li>
+              
               
                
             
--- src/components/ProfileApp/ProfileApp.tsx
+++ src/components/ProfileApp/ProfileApp.tsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React,{useState} from "react";
 import style from './ProfileApp.module.css'
+import closed from './cancel.png'
 
 
 
-export const ProfileApp:React.FC = () => {
+export const ProfileApp: React.FC = () => {
 
-    return(
+const [isOpen,setIsOpen] = useState(false)
+
+const toggleOpen = ()=>{
+    setIsOpen(!isOpen)
+}
+
+    return (
 
         <div className={style.profile_header}>
-          
-            <div><button className={style.btn}>
-                Sign In
-                </button></div>
+
+            <div><button className={style.btn}
+            onClick={()=>setIsOpen(true)}
+            >
+                Sign in
+            </button>
+            
+            {isOpen && (
+                <div className={style.modal}>
+                <div>
+                    <form className={style.modal_window} >
+                        <h1>Вход</h1>
+                        <input type="text" placeholder='Введите логин' />
+                        <input type="text" placeholder='Введите пароль' />
+                        <button>Войти</button>
+                        <div className={style.exit_Btn} onClick={()=>toggleOpen()}><img src={closed}/></div>
+                    </form>
+                </div>
+    
+            </div>
+            )}
+            </div>
         </div>
     )
 }
